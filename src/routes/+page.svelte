cc3607416c93eec852f0e2a847effcd6c55362f3
anbork <tonboruz@gmail.com>
2024-02-14 00:44:38 +0300

    Update with inlang paraglide

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import * as m from "$paraglide/messages"
+</script>
+
+<h1>{ m.hello_world() }</h1>
+<p>{ m.greeting({ name: 'Anton' }) }</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { paraglide } from "@inlang/paraglide-js-adapter-sveltekit/vite"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+    sveltekit(),
+		paraglide({
+			project: "./project.inlang",
+			outdir: "./src/paraglide",
+		}),
+  ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
