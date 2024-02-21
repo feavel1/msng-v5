f0d5c6d8327eb60dc9c5b9b17cdea2a99cdb0936
Baby Feavel <feavel@qq.com>
2024-02-21 20:11:03 +0800

    add a route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  import * as m from "$paraglide/messages"
+  import * as m from "$paraglide/messages";
 </script>
 
-<h1>{ m.hello_world() }</h1>
-<p>{ m.greeting({ name: 'Mike' }) }</p>
+<h1>{m.hello_world()}</h1>
+<p>{m.greeting({ name: "Mike" })}</p>
+
+<a href="abc">abc</a>
