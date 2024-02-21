f53a6d6ea7255e86e072723bfe1c35c55073e6c5
Baby Feavel <feavel@qq.com>
2024-02-21 20:06:00 +0800

    ok lets see

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
-	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit'
+  import { ParaglideJS } from "@inlang/paraglide-js-adapter-sveltekit";
   import { availableLanguageTags } from "$paraglide/runtime";
   import { page } from "$app/stores";
-	import { i18n } from '$lib/i18n'
+
+  import { i18n } from "$lib/i18n";
 </script>
 
 <ParaglideJS {i18n}>
-	<slot />
+  <div>
+    {#each availableLanguageTags as lang}
+      <a
+        class="capitalize btn variant-filled-primary"
+        href={i18n.route($page.url.pathname)}
+        hreflang={lang}
+        data-sveltekit-preload-data="off"
+      >
+        {lang}
+      </a>
+    {/each}
+  </div>
 
-	{#each availableLanguageTags as lang}
-		<a href={i18n.route($page.url.pathname)} hreflang={lang}>{lang}</a> &nbsp;
-	{/each}
+  <slot />
 </ParaglideJS>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
-import { paraglide } from "@inlang/paraglide-js-adapter-sveltekit/vite"
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
+import { paraglide } from "@inlang/paraglide-js-adapter-sveltekit/vite";
 
 export default defineConfig({
-	plugins: [
-		paraglide({
-			project: "./project.inlang",
-			outdir: "./src/paraglide",
-		}),
-		sveltekit()
+  plugins: [
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+    sveltekit(),
   ],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
 });
